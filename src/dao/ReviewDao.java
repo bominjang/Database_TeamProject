da5b2301c45c0eca21b9b157911cd9b873ba4f31
rrabit42 <gegiraffe@gmail.com>
2021-06-05 21:32:07 +0900

    [feat] 영화 별점 update transaction 구현

--- src/dao/ReviewDao.java
+++ src/dao/ReviewDao.java
@@ -5,6 +5,7 @@ import models.Reviews;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class ReviewDao {
 
@@ -20,15 +21,23 @@ public class ReviewDao {
     private static Connection conn;
     private static PreparedStatement pstmt;
     private static ResultSet rs;
-    private String sql;
+    private String sql, sql2, sql3;
 
 
     public int insert(String movie, String nickname, float rating, String detail) {
         sql = "INSERT INTO DB2021_Review(movie, nickname, create_time, rating, detail)";
         sql += " VALUES(?, ?, now(), ?, ?)";
 
+        // id면 좋겠지만
+        sql2 = "SELECT AVG(rating) as average FROM DB2021_Review WHERE movie = ?";
+        sql3 = "UPDATE DB2021_Movie set rating = ? WHERE title = ?";
+
         conn = DBConnection.getConnection();
         try {
+
+            conn.setAutoCommit(false);
+
+            // 리뷰 insert
             int returnCnt = 0;
             pstmt = conn.prepareStatement(sql);
             pstmt.setString(1, movie);
@@ -37,8 +46,40 @@ public class ReviewDao {
             pstmt.setString(4, detail);
             returnCnt = pstmt.executeUpdate();
 
+            // 트랜잭션-영화 평점 같이 올리기
+            // 영화 평점 다시 계산
+            pstmt = conn.prepareStatement(sql2);
+            pstmt.setString(1, movie);
+            rs = pstmt.executeQuery();
+            float update_rating;
+            if (rs.next()) {
+                update_rating = rs.getFloat("average");
+            } else {
+                throw new Exception();
+            }
+            
+            // 새 평점으로 업데이트
+            pstmt = conn.prepareStatement(sql3);
+            pstmt.setFloat(1, update_rating);
+            pstmt.setString(2, movie);
+            returnCnt = pstmt.executeUpdate();
+            conn.commit();
+
+            conn.setAutoCommit(true);
             conn.close();
             return returnCnt;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            try{
+                conn.rollback();
+            } catch(SQLException se) {
+                System.out.println(se.getMessage());
+            }
+        }
+
+        try {
+            conn.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
