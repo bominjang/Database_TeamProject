bc3e90b97d22cbd13a6874d196239c6f1aaf87d5
rrabit42 <gegiraffe@gmail.com>
2021-06-07 12:50:24 +0900

    [feat] create, drop sql

---
+++ drop.sql
@@ -0,0 +1,24 @@
+use DB2021Team03;
+
+drop table db2021_review;
+
+drop table db2021_actor_prize;
+
+drop table db2021_actor_movie;
+
+drop table db2021_director_prize;
+
+drop table db2021_actor;
+
+drop table db2021_movie;
+
+drop table db2021_director;
+
+drop table db2021_user;
+
+
+
+DROP VIEW titleExist;
+DROP VIEW DnameExist;
+DROP VIEW AnameExist;
+
